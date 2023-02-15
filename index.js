e862c72f210c41198459a59ecbbbd8562569ef93
Pinx <pinxisme.business@gmail.com>
2023-02-15 12:24:22 +0600

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,8 +0,0 @@
-<script>
-  function getTime() {
-    var d = new Date();
-    return d.getTime().toString();
-  }
-  document.write(getTime());
-</script>
-This code defines a JavaScript function that gets the current time in milliseconds and returns it
