6f913cddcff9ee9cacfe728c2f98f140433e0a78
Pinx <pinxisme.business@gmail.com>
2023-02-21 12:49:30 +0600

    Update time.js

--- time.js
+++ time.js
@@ -1,7 +1,11 @@
-<script>
-  function getTime() {
-    var d = new Date();
-    return d.getTime().toString();
-  }
-  document.write(getTime());
-</script>
+app.get('/time', (req, res) => {
+  // Get the current time
+  const date = new Date();
+  const h = date.getHours();
+  const m = date.getMinutes();
+  const s = date.getSeconds();
+  const time = h + ":" + m + ":" + s;
+
+  // Return the current time
+  res.send({ time });
+});
